b65b8ec79492af24f5102229bc4df7960601e043
itsnexverra <itsnexverra@gmail.com>
2026-01-26 22:24:25 +0530

    Update constant.tsx

--- constant.tsx
+++ constant.tsx
@@ -1,17 +1,166 @@
 export const products = [
   {
+    "id": "01a5eaea-631c-49fc-8c19-f04cfa1f3f83",
+    "title": "Biz Bridge",
+    "description": "dkfksj",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55026115027_5fe7df024c_k.jpg",
+      "https://live.staticflickr.com/65535/55027196768_84841aedc3_k.jpg",
+      "https://live.staticflickr.com/65535/55027265454_5b1519382f_k.jpg",
+      "https://live.staticflickr.com/65535/55026118122_1c3ce4104f_k.jpg"
+    ],
+    "price": 25000,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
+    "id": "48403817-aa80-4eb3-a175-99a2faa930b8",
+    "title": "Data Vision",
+    "description": "dkfksj",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55027405650_e80bdd31d5_k.jpg",
+      "https://live.staticflickr.com/65535/55027076396_4d6a9ae177_k.jpg",
+      "https://live.staticflickr.com/65535/55027076421_bf88901195_k.jpg",
+      "https://live.staticflickr.com/65535/55026180847_85a6cf248c_k.jpg"
+    ],
+    "price": 15000,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
+    "id": "31c14a7d-521a-4c0a-93b1-66215cbb832b",
+    "title": "Nexus",
+    "description": "dkfjk",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55027445460_dae12939b1_k.jpg",
+      "https://live.staticflickr.com/65535/55026220747_92a85bb3f2_k.jpg",
+      "https://live.staticflickr.com/65535/55027368204_7a75e78b65_k.jpg",
+      "https://live.staticflickr.com/65535/55027368184_0044381869_k.jpg"
+    ],
+    "price": 20000,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
+    "id": "da0f9d71-d418-4e83-acce-a23a7c3372ef",
+    "title": "Nexus Sphere",
+    "description": "dlfkd",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55054032524_cf05af356d_k.jpg",
+      "https://live.staticflickr.com/65535/55054130695_878ce8c8f2_k.jpg",
+      "https://live.staticflickr.com/65535/55052878607_b0182a6831_k.jpg",
+      "https://live.staticflickr.com/65535/55053781826_437e9e2d8a_k.jpg"
+    ],
+    "price": 17999.97,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
+    "id": "20afcd30-fe67-4481-9015-c14e32a3ee56",
+    "title": "OrbitDesk",
+    "description": "kjfksf",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55054204055_cb06a2b5c9_k.jpg",
+      "https://live.staticflickr.com/65535/55052952492_6a1ab601db_k.jpg",
+      "https://live.staticflickr.com/65535/55053855616_dc80059c1b_k.jpg",
+      "https://live.staticflickr.com/65535/55053855611_9cd6748112_k.jpg"
+    ],
+    "price": 16999.99,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
+    "id": "07d157b2-55ce-467e-b77f-263a3fbd25da",
+    "title": "DigiSphere",
+    "description": "kjsd",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55054120024_a3dd12f6a9_k.jpg",
+      "https://live.staticflickr.com/65535/55054049458_ecd83f1376_k.jpg",
+      "https://live.staticflickr.com/65535/55052967267_655fe7d351_k.jpg",
+      "https://live.staticflickr.com/65535/55054120019_efe79f01d5_k.jpg"
+    ],
+    "price": 21999.98,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null,
+    "wishlisted": false
+  },
+  {
+    "id": "525afd1b-f03d-4217-923a-c6270c7723e1",
+    "title": "MarketOne Business Dashboard",
+    "description": "kkdk",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55052789247_3ffee85f45_k.jpg",
+      "https://live.staticflickr.com/65535/55053694831_b4e90e897b_k.jpg",
+      "https://live.staticflickr.com/65535/55052792822_8b527488b9_h.jpg",
+      "https://live.staticflickr.com/65535/55053694841_f2db44c1f4_k.jpg"
+    ],
+    "price": 35000,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
+    "id": "8f582f86-020c-49b4-a983-abe5477ff62a",
+    "title": "NexusCore Business Dashboard",
+    "description": "lddsk",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55053710061_c506a219c2_k.jpg",
+      "https://live.staticflickr.com/65535/55052807817_48507a1814_k.jpg",
+      "https://live.staticflickr.com/65535/55052807812_afa6d8845d_k.jpg",
+      "https://live.staticflickr.com/65535/55053889628_d989c14ae7_k.jpg"
+    ],
+    "price": 34999.99,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
+    "id": "8ae205d2-259a-4292-9137-5a3d82fbffed",
+    "title": "Unified Business Dashboard",
+    "description": "kdfj",
+    "features": [],
+    "images": [
+      "https://live.staticflickr.com/65535/55053976399_66d364caac_k.jpg",
+      "https://live.staticflickr.com/65535/55053976389_33b7e9cee1_k.jpg",
+      "https://live.staticflickr.com/65535/55054074340_36859e904c_k.jpg",
+      "https://live.staticflickr.com/65535/55054074335_ea5cee5be2_k.jpg"
+    ],
+    "price": 34000,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  },
+  {
     "id": "c1d9a8b9-e18d-45d1-b0c8-10c5986196cd",
-    "title": "odoo dashboard",
+    "title": "Vertex Business Dashboard",
     "description": "sjkf",
     "features": [
       "chat"
     ],
     "images": [
-      "https://live.staticflickr.com/65535/55053976399_66d364caac_k.jpg"
+      "https://live.staticflickr.com/65535/55053733751_baa0c0a234_k.jpg",
+      "https://live.staticflickr.com/65535/55053913218_d2a496355c_k.jpg",
+      "https://live.staticflickr.com/65535/55054082930_666a218bda_k.jpg",
+      "https://live.staticflickr.com/65535/55053913223_8a3e92ea79_k.jpg"
     ],
-    "price": 38000,
+    "price": 40000,
     "category": "business",
     "type": "dashboard",
-    "downloadableFileName": "lumina-ebooks.zip"
+    "downloadableFileName": "lumina-ebooks.zip",
+    "wishlisted": false
   }
 ];
